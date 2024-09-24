087ba09b5f69f7211952bb0ec8ef8eab615dc395
Sudharsanan Velmurugan <sudharsananv.19eie@kongu.edu>
2024-09-24 11:56:07 +0530

    got ingnore

--- KaarvensBackend/obj/Debug/net8.0/KaarvensBackend.AssemblyInfo.cs
+++ KaarvensBackend/obj/Debug/net8.0/KaarvensBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaarvensBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f86a399c840e5377e74ea5349eb1f8d157434b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc31d141f086ac09c421457c9c5fa4084b6c8fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaarvensBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaarvensBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
